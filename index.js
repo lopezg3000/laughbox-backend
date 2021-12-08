bbf510a4811ac9d04af8095e3003ed641ccee357
Gabriel Lopez <lopezg3000@gmail.com>
2021-12-07 21:53:55 -0600

    port issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Sequelize = require('sequelize');
 const { messages } = require('./models');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
