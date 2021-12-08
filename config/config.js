3403dd5416346e8cd1c565fe09615f4be518de48
Gabriel Lopez <lopezg3000@gmail.com>
2021-12-07 21:07:54 -0600

    config changed to extension js

--- config/config.js
+++ config/config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+module.exports =
 {
   "development": {
     "username": "postgres",
@@ -18,6 +20,13 @@
     "password": "3e010321003b2316773f0cad50bfd6a72a02bb86194e96f8ec383f68ee10c92a",
     "database": "d2d87bg2l69u27",
     "host": "ec2-18-210-159-154.compute-1.amazonaws.com",
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "use_env_variable": "DATABASE_URL",
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false // <<<<<<< YOU NEED THIS
+      }
+    }
   }
 }
