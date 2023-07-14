d04d5b9873ff66214a0e708dc9130079e89a2bd9
Gabriel Lopez <lopezg3000@gmail.com>
2023-07-13 20:54:18 -0500

    export default instead of module.export

--- config/config.js
+++ config/config.js
@@ -1,33 +1,33 @@
 require('dotenv').config();
-module.exports =
-{
-  "development": {
-    "username": "postgres",
-    "password": "postgres",
-    "database": "messageBoard",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  },
-  "production": {
-    "host": process.env.PRODUCTION_HOST,
-    "username": process.env.PRODUCTION_USER,
-    "password": process.env.PRODUCTION_PASSWORD,
-    "database": process.env.DATABASE,
-    "dialect": "postgres",
-    // "database_url": process.env.DATABASE_URL
-    // "use_env_variable": "DATABASE_URL",
-    // "dialectOptions": {
-    //   "ssl": {
-    //     "require": true,
-    //     "rejectUnauthorized": false // <<<<<<< YOU NEED THIS
-    //   }
-    // }
+export default
+  {
+    "development": {
+      "username": "postgres",
+      "password": "postgres",
+      "database": "messageBoard",
+      "host": "127.0.0.1",
+      "dialect": "postgres"
+    },
+    "test": {
+      "username": "root",
+      "password": null,
+      "database": "database_test",
+      "host": "127.0.0.1",
+      "dialect": "postgres"
+    },
+    "production": {
+      "host": process.env.PRODUCTION_HOST,
+      "username": process.env.PRODUCTION_USER,
+      "password": process.env.PRODUCTION_PASSWORD,
+      "database": process.env.DATABASE,
+      "dialect": "postgres",
+      "database_url": process.env.DATABASE_URL
+      // "use_env_variable": "DATABASE_URL",
+      // "dialectOptions": {
+      //   "ssl": {
+      //     "require": true,
+      //     "rejectUnauthorized": false // <<<<<<< YOU NEED THIS
+      //   }
+      // }
+    }
   }
-}
