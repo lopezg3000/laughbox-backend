30d6eaf0e4adbdc1f38872f26727596cfeb63a3d
Gabriel Lopez <lopezg3000@gmail.com>
2023-07-19 00:09:55 -0500

    environmental variables rearanged

--- config/config.js
+++ config/config.js
@@ -16,10 +16,10 @@ module.exports =
     "dialect": "postgres"
   },
   "production": {
-    "host": process.env.PRODUCTION_HOST,
     "username": process.env.PRODUCTION_USER,
     "password": process.env.PRODUCTION_PASSWORD,
     "database": process.env.DATABASE,
+    "host": process.env.PRODUCTION_HOST,
     "dialect": "postgres",
     "use_env_variable": process.env.DATABASE_URL,
     "dialectOptions": {
