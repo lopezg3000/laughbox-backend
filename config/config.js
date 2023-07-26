778d6a9006d38a1943eb94019d099e780862fac7
Gabriel Lopez <lopezg3000@gmail.com>
2023-07-26 13:10:45 -0500

    development and test in config removed

--- config/config.js
+++ config/config.js
@@ -1,6 +1,20 @@
 require('dotenv').config();
 module.exports =
 {
+  // "development": {
+  //   "username": "postgres",
+  //   "password": "postgres",
+  //   "database": "messageBoard",
+  //   "host": "127.0.0.1",
+  //   "dialect": "postgres"
+  // },
+  // "test": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_test",
+  //   "host": "127.0.0.1",
+  //   "dialect": "postgres"
+  // },
   "production": {
     "username": process.env.PRODUCTION_USER,
     "password": process.env.PRODUCTION_PASSWORD,
@@ -14,19 +28,5 @@ module.exports =
         "rejectUnauthorized": false // <<<<<<< YOU NEED THIS
       }
     }
-  },
-  "development": {
-    "username": "postgres",
-    "password": "postgres",
-    "database": "messageBoard",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
   }
 }
